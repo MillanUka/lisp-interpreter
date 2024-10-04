1e12020659d8541339050dd9fc96f4f2c8b1019a
Millan Uka <ukamillan@gmail.com>
2024-10-04 21:10:49 +1300

    Add some changes

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -35,8 +35,22 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case 0:
-		tok.Literal = ""
-		tok.Type = token.EOF
+		tok = newToken(token.EOF, "EOF")
+	case '\n':
+		tok = newToken(token.NEWLINE, "\n")
+	case '(':
+		tok = newToken(token.LPAREN, "(")
+	case ')':
+		tok = newToken(token.RPAREN, ")")
+	case '.':
+		tok = newToken(token.DOT, ".")
+	case '-':
+	case '+':
+		// TODO
+	case '\'':
+		return newToken(token.QUOTE, "'")
+	default:
+		// TODO
 	}
 
 	l.readChar()
@@ -47,4 +61,12 @@ func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
 	}
+}
+
+func newToken(tokenType token.TokenType, literal string) token.Token {
+	return token.Token{Type: tokenType, Literal: literal}
+}
+
+func newAtom(atomName string) token.Token {
+	return newToken(token.ATOM, atomName)
 }
